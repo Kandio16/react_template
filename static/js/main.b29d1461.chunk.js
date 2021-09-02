60a129058943056e057a2b16174f9c3a6e090f98
Kandiak Oleg <kandio1604@gmail.com>
2021-09-02 13:10:07 +0300

    Updates

---
+++ static/js/main.b29d1461.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact_template=this.webpackJsonpreact_template||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);c(1);var n=c(3),r=c.n(n),s=(c(8),c(0));var a=function(){return Object(s.jsx)("div",{className:"App",children:Object(s.jsx)("h1",{children:"Hi"})})};r.a.render(Object(s.jsx)(a,{}),document.getElementById("root"))},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.b29d1461.chunk.js.map
